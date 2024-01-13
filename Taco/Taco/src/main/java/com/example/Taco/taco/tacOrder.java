4a88df7d7a5022f41a1eacc3f0c7d1ba297bb363
7thCoder <nuibima@gmail.com>
2024-01-13 13:10:52 +0100

    code

--- Taco/Taco/src/main/java/com/example/Taco/taco/tacOrder.java
+++ Taco/Taco/src/main/java/com/example/Taco/taco/tacOrder.java
@@ -26,7 +26,7 @@ import com.example.Taco.taco.Taco;
 
 @Data
 @Table
-public class tacOrder   implements Serializable {
+public class tacOrder  implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
