e02fb209183e179e360c72e0eb48beceaf013580
7thCoder <nuibima@gmail.com>
2024-01-25 23:18:01 +0100

    code

--- Taco/Taco/src/main/java/com/example/Taco/controller/DesignTacoController.java
+++ Taco/Taco/src/main/java/com/example/Taco/controller/DesignTacoController.java
@@ -39,36 +39,36 @@ public class DesignTacoController {
         this.ingredientRepo = ingredientRepo;
     }
 
-    @ModelAttribute
-    public void addIngredientsToModel (Model model){
-        Iterable <Ingredient> ingredients = ingredientRepo.findAll();
-        
-        Type[] types = Ingredient.Type.values();
-         for (Type type: types){
-             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients,type)); 
-         }
-    }
-
     // @ModelAttribute
     // public void addIngredientsToModel (Model model){
-    //     List<Ingredient> ingredients = Arrays.asList(
-    //         new Ingredient("FLTO","flour Tortilla",Type.WRAP),
-    //         new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-    //         new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-    //         new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-    //         new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-    //         new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-    //         new Ingredient("CHED", "Cheddar", Type.CHEESE),
-    //         new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-    //         new Ingredient("SLSA", "Salsa", Type.SAUCE),
-    //         new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    //     );
-
+    //     Iterable <Ingredient> ingredients = ingredientRepo.findAll();
+        
     //     Type[] types = Ingredient.Type.values();
-    //     for (Type type: types){
-    //         model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients,type)); 
-    //     }
+    //      for (Type type: types){
+    //          model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients,type)); 
+    //      }
     // }
+
+    @ModelAttribute
+    public void addIngredientsToModel (Model model){
+        List<Ingredient> ingredients = Arrays.asList(
+            new Ingredient("FLTO","flour Tortilla",Type.WRAP),
+            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+            new Ingredient("CHED", "Cheddar", Type.CHEESE),
+            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+            new Ingredient("SLSA", "Salsa", Type.SAUCE),
+            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type: types){
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients,type)); 
+        }
+    }
 
         
     @ModelAttribute(name = "tacOrder")
--- Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
+++ Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
@@ -1,31 +1,31 @@
-// package com.example.Taco.controller;
+package com.example.Taco.controller;
 
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.data.domain.Sort;
-// import org.springframework.data.domain.jaxb.SortAdapter;
-// import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.jaxb.SortAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.example.Taco.taco.Taco;
-// import com.example.Taco.taco.tacOrder;
+import com.example.Taco.taco.Taco;
+import com.example.Taco.taco.tacOrder;
 
 
-// @RestController
-// @RequestMapping(path = "api/tacos", produces = "applcation/json")
-// @CrossOrigin(origins = "http: //tacocloud:8080")
-// public class TacoController {
-//     private tacOrder tacorepo;
+@RestController
+@RequestMapping(path = "api/tacos", produces = "applcation/json")
+@CrossOrigin(origins = "http: //tacocloud:8080")
+public class TacoController {
+    private tacOrder tacorepo;
 
-//     public TacoController(tacOrder tacoRepo){
-//         this.tacorepo = tacoRepo;
-//     }
+    public TacoController(tacOrder tacoRepo){
+        this.tacorepo = tacoRepo;
+    }
 
-//     // @GetMapping(params = "recent")
-//     // public Iterable<Taco> recentTacos(){
-//     //     PageRequest page = PageRequest.of(
-//     //         0, 12, Sort.by("createdAt").descending());
-//     //     return tacorepo.findAll(page).getContent();
-//     // }
-// }
+    // @GetMapping(params = "recent")
+    // public Iterable<Taco> recentTacos(){
+    //     PageRequest page = PageRequest.of(
+    //         0, 12, Sort.by("createdAt").descending());
+    //     return tacorepo.findAll(page).getContent();
+    // }
+}
