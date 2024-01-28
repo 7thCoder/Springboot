79c805f94e5ccd335cb80c2261f4366c62ba7411
7thCoder <nuibima@gmail.com>
2024-01-28 01:30:52 +0100

    added http status code response

--- Taco/Taco/src/main/java/com/example/Taco/Repository/tacoRepository.java
+++ Taco/Taco/src/main/java/com/example/Taco/Repository/tacoRepository.java
@@ -1,10 +1,17 @@
 package com.example.Taco.Repository;
 
-// import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.Taco.taco.Taco;
 
 public interface tacoRepository extends PagingAndSortingRepository <Taco, Long> {
-    
+    //pagind and sorting repository is used for pagination and sorting
+
+    @Query("SELECT u FROM Taco u WHERE u.id = :id")
+    Optional<Taco>findById(@Param("id") Long id);
 }
--- Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
+++ Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
@@ -1,9 +1,14 @@
 package com.example.Taco.controller;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +19,7 @@ import com.example.Taco.taco.Taco;
 
 @RestController
 @RequestMapping(path="/api/tacos", produces="application/json")
-@CrossOrigin(origins="http://tacocloud:8080")  
+@CrossOrigin(origins="http://localhost:8080")  
 public class TacoController{
     
     private tacoRepository tacoRepo;
@@ -27,6 +32,18 @@ public class TacoController{
     public Iterable<Taco> recentTacos() {
         PageRequest page = PageRequest.of(1, 12, Sort.by("createdAt").descending());
         return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+
+        if(optTaco.isPresent()){
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+
     }
 
 }
