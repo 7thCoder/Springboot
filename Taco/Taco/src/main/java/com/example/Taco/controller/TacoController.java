944181884a87afee93d6aed58391339e019d10e4
7thCoder <nuibima@gmail.com>
2024-02-02 20:23:35 +0100

    added rest dependency and default api base path

--- Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
+++ Taco/Taco/src/main/java/com/example/Taco/controller/TacoController.java
@@ -1,59 +1,72 @@
-package com.example.Taco.controller;
+// package com.example.Taco.controller;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.dao.EmptyResultDataAccessException;
+// import org.springframework.data.domain.PageRequest;
+// import org.springframework.data.domain.Sort;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Taco.Repository.tacoRepository;
-import com.example.Taco.taco.Taco;
+// import com.example.Taco.Repository.tacoRepository;
+// import com.example.Taco.taco.Taco;
 
 
 
-@RestController
-@RequestMapping(path="/api/tacos", produces="application/json")
-@CrossOrigin(origins="http://localhost:8080")  
-public class TacoController{
+// @RestController
+// @RequestMapping(path="/api/tacos", produces="application/json")
+// @CrossOrigin(origins="http://localhost:8080")  
+// public class TacoController{
     
-    private tacoRepository tacoRepo;
+//     private tacoRepository tacoRepo;
 
-    public TacoController(tacoRepository tacoRepo){
-        this.tacoRepo = tacoRepo;
-    }
+//     public TacoController(tacoRepository tacoRepo){
+//         this.tacoRepo = tacoRepo;
+//     }
 
-    @GetMapping(params ="recent")
-    public Iterable<Taco> recentTacos() {
-        PageRequest page = PageRequest.of(1, 12, Sort.by("createdAt").descending());
-        return tacoRepo.findAll(page).getContent();
-    }
+//     @GetMapping(params ="recent")
+//     public Iterable<Taco> recentTacos() {
+//         PageRequest page = PageRequest.of(1, 12, Sort.by("createdAt").descending());
+//         return tacoRepo.findAll(page).getContent();
+//     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
-        Optional<Taco> optTaco = tacoRepo.findById(id);
+//     @GetMapping("/{id}")
+//     public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+//         Optional<Taco> optTaco = tacoRepo.findById(id);
 
-        if(optTaco.isPresent()){
-            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
-        }
+//         if(optTaco.isPresent()){
+//             return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+//         }
 
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+//         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 
-    }
+//     }
 
-    @PostMapping(consumes = "application/json")
-    @ResponseStatus (HttpStatus.CREATED)
-    public Taco postTaco(@RequestBody Taco taco){
-        return tacoRepo.save(taco);
-    }
+//     @PostMapping(consumes = "application/json")
+//     @ResponseStatus (HttpStatus.CREATED)
+//     public Taco postTaco(@RequestBody Taco taco){
+//         return tacoRepo.save(taco);
+//     }
+
+//     @DeleteMapping("/{orderid}")
+//     @ResponseStatus(HttpStatus.NO_CONTENT)
+//     public void deleteOrder(@PathVariable("orderid") Long orderId){
+//         try{
+//             tacoRepo.deleteById(orderId);
+//         }
+//         catch(EmptyResultDataAccessException e){
+
+//         }
+//     }
 
 
-}
+// }
