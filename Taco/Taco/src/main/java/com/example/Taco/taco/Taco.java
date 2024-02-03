87f6949d4af5fad802c160715c6ac29c2ef675fe
7thCoder <nuibima@gmail.com>
2024-02-03 13:47:57 +0100

    added restresource (default api base path)

--- Taco/Taco/src/main/java/com/example/Taco/taco/Taco.java
+++ Taco/Taco/src/main/java/com/example/Taco/taco/Taco.java
@@ -3,6 +3,8 @@ package com.example.Taco.taco;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +18,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@RestResource(rel = "tacos", path = "tacos")
 public class Taco {
 
     @Id
